5f46e9f7fe0936eb10417c1da583cb68c91032c1
keith kj <kgkj1995@hotmail.com>
2018-08-30 19:17:49 +0800

    g

--- DystopiaEngine/DystopiaEngine/Source/Component/RigidBody.cpp
+++ DystopiaEngine/DystopiaEngine/Source/Component/RigidBody.cpp
@@ -61,11 +61,33 @@ namespace Dystopia
 		//AddForce(_force, _point, mOwnerTransform->GetGlobalPosition());
 	}
 
+	/*
+	/brief
+		Add a force at a certain position of the object.
+		This will create a angular velocity that will cause the object to rotate
+		& also move the object in the direction the force is applied
+
+	/param _force
+		The Amount of force to be applied to the object
+
+	/param _point
+		The point at which the force is applied to.
+		_point should be in global coordinate
+
+	/param _origin
+		The pivot/origin position of the object
+		_origin should be in global coordinate
+
+	*/
 	void RigidBody::AddForce(Math::Vec3D const & _force, Math::Point3D const & _point, Math::Point3D const & _origin)
 	{
+		/*Vector from origin/pivot of the object to the _point where the force is applied*/
 		Math::Vec3D && displacement_v = _point - _origin;
+		/*Generate the angular velocity as a result of the force being applied at _point*/
 		Math::Vector3D Angular = Cross(_force, displacement_v) / displacement_v.MagnitudeSqr();
+		/*Add to the total Anuglar velocity of the object in this Update Frame*/
 		mAngularVelocity  += Angular;
+		/*Add the the total Linear Veloctiy of the object in this Update Frame*/
 		mCumulativeVector += _force;
 	}
 
