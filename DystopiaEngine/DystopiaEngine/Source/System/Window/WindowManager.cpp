5d2631e39bef632052a9ff9f07045e0ddc6bf55e
Aaron Chu <m.chu@digipen.edu>
2018-09-17 20:51:17 +0800

    small changes

--- DystopiaEngine/DystopiaEngine/Source/System/Window/WindowManager.cpp
+++ DystopiaEngine/DystopiaEngine/Source/System/Window/WindowManager.cpp
@@ -37,8 +37,8 @@ namespace
 	constexpr bool	DEFAULT_FULLSCREEN		= false;
 	constexpr int	DEFAULT_WIDTH			= 1600;
 	constexpr int	DEFAULT_HEIGHT			= 900;
-	constexpr int	LOGO_WIDTH				= 620;
-	constexpr int	LOGO_HEIGHT				= 300;
+	constexpr int	LOGO_WIDTH				= 992;
+	constexpr int	LOGO_HEIGHT				= 480;
 
 	Dystopia::MouseData* pMouse = nullptr;
 
