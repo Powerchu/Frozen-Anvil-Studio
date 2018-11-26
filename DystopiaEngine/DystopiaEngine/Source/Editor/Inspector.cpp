f5e01b059c1ee745bf01c7c9ff0bd24b3bc76b93
m.chu <m.chu@DIT0494SG>
2018-11-26 20:18:04 +0800

    btree

--- DystopiaEngine/DystopiaEngine/Source/Editor/Inspector.cpp
+++ DystopiaEngine/DystopiaEngine/Source/Editor/Inspector.cpp
@@ -139,10 +139,13 @@ namespace Editor
 				auto nFn = cmd->MakeFnCommand(&Dystopia::GameObject::SetName, HashString{ buffer });
 				cmd->FunctionCommand(mpFocus->GetID(), oFn, nFn);
 			}
+
+			//TODO:
 			if (EGUI::Display::DropDownSelection("Tag", i, mpFocus->GetAllTags_str(), 32))
 			{
 
 			}
+
 			EGUI::SameLine();
 			EGUI::ChangeAlignmentYOffset(0);
 			j = (mpFocus->GetFlags() & FLAG_LAYER_WORLD) ? 1 : (mpFocus->GetFlags() & FLAG_LAYER_UI) ? 2 : 0;
