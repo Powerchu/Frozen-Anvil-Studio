9dc1b53fb318f056586a649c160c8d42e3e92701
keith.goh <keith.goh@DIT0489SG>
2019-01-27 18:47:08 +0800

    Fixed Camera

--- DystopiaEngine/DystopiaEngine/Source/Component/Camera.cpp
+++ DystopiaEngine/DystopiaEngine/Source/Component/Camera.cpp
@@ -165,10 +165,10 @@ void Dystopia::Camera::SetPerspective(Math::Angle _fFOV, float _fAspectRatio, fl
 	float fDist = 1.f / std::tanf(_fFOV.Radians() * .5f);
 
 	mProjection = Math::Matrix4{
-		fDist * _fAspectRatio, .0f		, .0f				, .0f,
-		.0f                  , -fDist	, .0f				, .0f,
-		.0f                  , .0f		, -2.f * fDistZ		, (_fFar + _fNear) * fDistZ,
-		.0f                  , .0f		, -1.f				, .0f
+		fDist * _fAspectRatio, .0f		, .0f				           , .0f,
+		.0f                  , -fDist	, .0f				           , .0f,
+		.0f                  , .0f						, -(_fFar + _fNear) * fDistZ   , -2.f * _fFar * _fNear * fDistZ,
+		.0f                  , .0f		, -1.f				           , .0f
 	};
 }
 
@@ -181,7 +181,7 @@ void Dystopia::Camera::SetPerspective(float _fLeft, float _fRight, float _fTop, 
 	mProjection = Math::Matrix4{
 		(2.f * _fNear) * fWidth, .0f						, (_fRight + _fLeft) * fWidth  , .0f,
 		.0f                    , -(2.f * _fNear) * fHeight	, (_fTop + _fBottom) * fHeight , .0f,
-		.0f                    , .0f						, -(_fFar + _fNear) * fDistZ   , 2.f * _fFar * _fNear * fDistZ,
+		.0f                    , .0f						, -(_fFar + _fNear) * fDistZ   , -2.f * _fFar * _fNear * fDistZ,
 		.0f                    , .0f						, -1.f                         , .0f
 	};
 }
@@ -215,7 +215,7 @@ void Dystopia::Camera::SetCamera(void)
 		//	.0f, .0f, .0f, 1.f
 		//}; // Screen space -> Viewport space -> projection space
 
-	mView.AffineInverse();
+	mView = mView.AffineInverse();
 }
 
 void Dystopia::Camera::SetDebugDraw(bool _bDebugDraw) noexcept
